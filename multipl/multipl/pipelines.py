5c47adb6e3dc188368ebaabc9439c2875e245ea0
Ryan Houck <ryan@levelskies.com>
2015-07-26 13:02:10 -0700

    added pipline file to validate item and write to json file in data directory

--- multipl/multipl/pipelines.py
+++ multipl/multipl/pipelines.py
@@ -4,8 +4,29 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime
+import json
+
+import dateutil.parser
+from scrapy.exceptions import DropItem
 
 
-class MultiplPipeline(object):
+class ValidatePipeline(object):
     def process_item(self, item, spider):
+        try:
+            dateutil.parser.parse(item['date']).date()
+            return item
+        except:
+            raise DropItem("Not valid date")
+
+class JsonWriterPipeline(object):
+
+    def __init__(self):
+        today = str(datetime.datetime.now().date()).replace("-", "_")
+        fn = '../data/multipl_{0}.jl'.format(today)
+        self.file = open(fn, 'wb')
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
         return item
--- multipl/multipl/settings.py
+++ multipl/multipl/settings.py
@@ -61,9 +61,10 @@ NEWSPIDER_MODULE = 'multipl.spiders'
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'multipl.pipelines.SomePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'multipl.pipelines.ValidatePipeline': 300,
+   'multipl.pipelines.JsonWriterPipeline': 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
